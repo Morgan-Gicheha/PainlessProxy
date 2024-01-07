3bc11652849da98a228a13d5f46b3a14e25bdd18
Morgan Gicheha <morgangicheha4@gmail.com>
2024-01-07 12:10:30 +0300

    merge commit

--- code/routes/mainRoute.ts
+++ code/routes/mainRoute.ts
@@ -1,8 +1,12 @@
 import express, { Router, Request, Response } from "express";
+import { IncomingRequest } from "../utils/schemas/interface";
 
 const router: Router = express.Router();
 
 router.post("/", (req: Request, res: Response) => {
+    // get url to call
+    const data:IncomingRequest = req.body
+    
     res.send({ hello: "world" });
 });
 
