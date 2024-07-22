7c2b52827c81e6ad9531c637afeb6f683eb6bcf9
Morgan Gicheha <morgangicheha4@gmail.com>
2024-07-22 15:14:52 +0300

    handling xml

--- code/index.ts
+++ code/index.ts
@@ -1,26 +1,13 @@
-import express, { Express } from "express";
+import express, { Express, Request, Response, Router } from "express";
 import { router as callbackRouter } from "./routes/callback";
 import { router as mainRouter } from "./routes/mainRoute";
 import { router as registryRouter } from "./routes/registry";
 import mongoose from "mongoose";
-import bodyParserXml from "body-parser-xml";
 
 import { config } from "./utils/configs/configs";
 import { connectToMongoDB } from "./models/con.db.mongo";
 
 const app: Express = express();
-
-// Configure XML body parser
-bodyParserXml(express);
-app.use(
-    bodyParserXml({
-        xml: {
-            limit: "1MB", // Adjust the limit as needed
-            explicitArray: false,
-            explicitRoot: false,
-        },
-    })
-);
 
 app.use(express.json());
 
